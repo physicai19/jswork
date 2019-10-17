650c72865960a866603bfde012221062d41a34cc
physicai19 <2068533286@qq.com>
2019-10-18 01:07:17 +0800

    work6

--- work6/pai.js
+++ work6/pai.js
@@ -1,29 +1,32 @@
 function calculate(){
-    let num = document.getElementById().value 
+    let num = document.getElementById('num').value
     num = parseInt(num)&&Number(num)
     if(!isNaN(num)){
-        document.getElementById().value = pi()
+        document.getElementById('pai').value=pi(num)
     }
 }
 function factorial(n){
-    let sum = 1
-    for(let i=0;i<=num;++i){
-    sum += arr[i] 
-    }
-    return sum 
-}
-function oddFactoria(n){
-    let sum = 1
-    for(let i=1;i < arr.length;++i){
-    sum   
+    let sum =1
+    for(let i=1;i<=n;++i){
+        sum *=i
     }
     return sum
+}
+function oddFactorial(n){
+    let sum =1
+    for(let i=1;i<=n;++i){
+        sum *= 2*i+1
+}
+return sum
 }
 function pi(num){
-    let sum = 0
+    let sum =0
     for(let i=0;i<=num;++i){
-        let dividend = factorial()
-    }
-    return sum
+    let dividend =factorial(i)
+    let divisor = oddFactorial(i)
+    sum += (dividend/divisor)
+
+}
+return sum * 2
 }
 calculate()
