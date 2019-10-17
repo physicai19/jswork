2f0a3f5406e0035b8d372ad6a851fdcf51b2c455
physicai19 <2068533286@qq.com>
2019-10-17 23:00:45 +0800

    work6

---
+++ work6/pai.js
@@ -0,0 +1,29 @@
+function calculate(){
+    let num = document.getElementById().value 
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById().value = pi()
+    }
+}
+function factorial(n){
+    let sum = 1
+    for(let i=0;i<=num;++i){
+    sum += arr[i] 
+    }
+    return sum 
+}
+function oddFactoria(n){
+    let sum = 1
+    for(let i=1;i < arr.length;++i){
+    sum   
+    }
+    return sum
+}
+function pi(num){
+    let sum = 0
+    for(let i=0;i<=num;++i){
+        let dividend = factorial()
+    }
+    return sum
+}
+calculate()
