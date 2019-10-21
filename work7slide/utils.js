f5cde4546bb34f5c73dee4b151ec4e03f45f6418
physicai19 <2068533286@qq.com>
2019-10-21 20:29:57 +0800

    work7slide

--- work7slide/utils.js
+++ work7slide/utils.js
@@ -1,7 +1,7 @@
 function bubbleSort(str){
     let arr = str.split(',')
     let sortLog = []
-    for (let i = 0; i < arr.length - i - 1; i++){
+    for (let i = 0; i < arr.length - 1; i++){
         for (let j = 0; j < arr.length - i - 1; j++){
            sortLog.push([arr.concat(),[j, j + 1]])
            sortLog.push([arr.concat(),[j, j + 1]])
@@ -30,7 +30,7 @@ function showLog(logValues, showElement){
     if (pos[0] != -1)
     setTimeout("showLog(logValues,showDiv)",500)
 }
-function inserSort(str){
+function insertSort(str){
     let arr = str.split(',')
     let sortLog = []
     for (let i = 1; i < arr.length; i++){
