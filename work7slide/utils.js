e70df44bfd6136e1727334b4e775d1b6f7545cca
physicai19 <apple@jd505-51.local>
2019-10-21 11:57:02 +0800

    work7

---
+++ work7slide/utils.js
@@ -0,0 +1,21 @@
+function bubbleSort(str){
+    let arr = str.split(',')
+    let sortLog = []
+    for (let i = 0; i < arr.length - 1; i++){
+        for (let j = 0; j < arr.length - i - 1; j++){
+            sortLog.push([arr.concat(),[j, j = 1]])
+            sortLog.push([arr.concat(), [j, j + 1]])
+            if (arr[j] > arr[j + 1]){
+                [arr[j],arr[j + 1]] = [arr[j + 1],arr[j]]
+                sortLogpush([arr.concat(), [j, j = 1]])
+        }
+    }
+}
+sortLog.push([arr.concat(),[-1,-1]])
+return sortLog
+function showLog(logValues, showElement){
+    let str = ''
+    let{
+        done,
+    }
+}
