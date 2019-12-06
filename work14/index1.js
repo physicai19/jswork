9e3d3f8fd1e7344afc6b3414c3d4834edabbcefd
physicai19 <2068533286@qq.com>
2019-12-06 21:15:52 +0800

    work14

--- work14/index1.js
+++ work14/index1.js
@@ -1,43 +1,42 @@
-var lamp = {
-    red: {
-        obj: document.getElementById('red'),
-        timeout: 3,
-        style: ['red','gray','gray'],
-        next: 'green'
+var lamp={
+    red:{
+        obj:document.getElementById('red'),
+        timeout:3,
+        style:['red','gray','gray'],
+        next:'green'
     },
-    yellow: {
-        obj: document.getElementById('yellow'),
-        timeout: 3,
-        style: ['gray','yellow','gray'],
-        next: 'red'
+    yellow:{
+        obj:document.getElementById('yellow'),
+        timeout:2,
+        style:['gray','yellow','gray'],
+        next:'red'
     },
-    green: {
-        obj: document.getElementById('green'),
-        timeout: 3,
-        style: ['gray','gray','green'],
-        next: 'yellow'
+    green:{
+        obj:document.getElementById('green'),
+        timeout:5,
+        style:['gray','gray','green'],
+        next:'yellow'
     },
-    changeStyle(style) {
-        this.red.obj.className = style[0];
-        this.yellow.obj.className = style[1];
-        this.green.obj.className = style[2];
+    changeStyle(style){
+        this.red.obj.className =style[0];
+        this.yellow.obj.className =style[1];
+        this.green.obj.className =style[2];
     }
 };
-var count = {
+var count ={
     obj: document.getElementById('count'),
-    change: function(num) {
-        this.obj.innerHTML = (num < 10) ? ('0' + num) : num;
+    change: function(num){
+        this.obj.innerHTML =(num <10) ?('0' +num):num;
     }
 };
 var now = lamp.green;
-var timeout = now.timeout;
+var timeout= now.timeout;
 lamp.changeStyle(now.style);
 count.change(timeout);
-setInterval(function() {
-    if (--timeout <= 0) {
-        now == lamp[now.next];
-        timeout = now.timeout;
+setInterval(function(){
+    if(--timeout <=0){
+        now =lamp[now.next];
+        timeout= now.timeout;
         lamp.changeStyle(now.style);
     }
-    count.change(timeout);
-}, 1000);
+ count.change(timeout);},1000);
